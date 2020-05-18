464f5d574714225251626598e64e208bd8175001
Fred <fredmartinsw@gmail.com>
2020-05-18 00:22:16 -0300

    Alteração do email para contato

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -11,10 +11,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "rafarrmends@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = "mundomagico.arrozpapel@gmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Contato pelo site, Mundo Mágico:  $name";
 $body = "Você recebeu uma mensagem pelo formulário de contato\n\n"."Aqui está os detalhes:\n\nNome: $name\n\nEmail: $email\n\nTelefone: $phone\n\nMensagem:\n$message";
-$header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: mundomagico.arrozpapel@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Responda para: $email";	
 
 if(!mail($to, $subject, $body, $header))
